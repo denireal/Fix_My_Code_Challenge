426b9a9aa7e80c261879d847e295f546ef4ef010
denireal <denyefa.wari@federalpolyekowe.edu.ng>
2024-02-22 12:13:13 -0800

    updating task

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,34 +1,24 @@
 #!/usr/bin/node
 /*
-This script prints a square made of '#' characters with a specified size.
-
-Usage: ./1-print_square.js <size>
-Example: ./1-print_square.js 8
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
 */
 
-/* Check if a command-line argument is provided */
-if (process.argv.length <= 2)
-{
-/* Print error message & usage instructions if no argument is provided */
-process.stderr.write("Missing argument\n");
-process.stderr.write("Usage: ./1-print_square.js <size>\n");
-process.stderr.write("Example: ./1-print_square.js 8\n");
-/* Exit with error code 1 */
-process.exit(1);
+if (process.argv.length <= 2) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1);
 }
 
-/* Parse the command-line argument as an integer */
 size = parseInt(process.argv[2], 10);
 
-/* Loop to print the square */
-for (let row = 0 ; row < size ; row++)
-{
-/* Loop to print each row of the square */
-for (let column = 0 ; column < size ; column++)
-{
-/* Print '#' character */
-process.stdout.write("#");
-}
-/* Move to the next line after printing each row */
-process.stdout.write("\n");
+for (let i = 0 ; i < size ; i ++) {
+    let row = '';
+    for (let j = 0 ; j < size ; j ++) {
+        row += "#";
+    }
+    console.log(row);
 }
